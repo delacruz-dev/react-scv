2f7f6cf0cb1b0b3079d4e9e18c76444aa3bc362a
Marcello Montemagno <marcello.montemagno@workshare.com>
2017-05-03 17:34:51 +0100

    basic flow support

--- config/babel.js
+++ config/babel.js
@@ -3,6 +3,7 @@
 module.exports = {
   presets: [
     require.resolve('babel-preset-es2015'),
+    require.resolve('babel-preset-flow'),
     require.resolve('babel-preset-stage-0'),
     require.resolve('babel-preset-react')
   ]
