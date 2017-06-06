29b27898d83556f8faade0707c50d348139040d8
Marcello Montemagno <marcello.montemagno@workshare.com>
2017-06-06 18:10:59 +0100

    skipping uninstalled dependencies during dll build

--- config/webpack.dll.js
+++ config/webpack.dll.js
@@ -3,18 +3,31 @@
 const core = require('./webpack.core');
 const merge = require('webpack-merge');
 const path = require('path');
+const fs = require('fs');
 const webpack = require('webpack');
 
 const CWD = process.cwd();
 const BUILD = path.join(CWD, 'build');
 
+let dllModules = getOnlyInstalledModules(['cookies-js', 'mixpanel-browser', 'moment', 'query-string', 'react', 'react-addons-test-utils', 'react-datetime', 'react-dom', 'react-router', 'validator',
+    'babel-polyfill', 'whatwg-fetch', 'react-hot-loader', 'webpack-hot-middleware', 'sockjs-client', 'querystring-es3', 'ansi-html', 'html-entities', 'punycode', 'events']);
+
+function getOnlyInstalledModules (modules) {
+    let result = [];
+    modules.forEach((module) => {
+        const moduleFolderPath = path.join(process.cwd(), path.join('node_modules', module));
+        if (fs.existsSync(moduleFolderPath)) {
+            result.push(module);
+        }
+    });
+    return result;
+}
+
 const config = merge(core, {
     entry: {
-        'dev': [
-            'cookies-js', 'immutability-helper', 'lodash/cloneDeep', 'lodash/merge', 'lodash/isEqual',
-            'mixpanel-browser', 'moment', 'query-string', 'react', 'react-addons-test-utils', 'react-datetime', 'react-dom', 'react-router', 'validator',
-            "babel-polyfill", "whatwg-fetch","react-hot-loader", "webpack-hot-middleware", 'sockjs-client', 'querystring-es3', 'ansi-html', 'html-entities', 'punycode', 'events'
-        ],
+        'dev': dllModules.concat([
+            'lodash/cloneDeep', 'lodash/merge', 'lodash/isEqual'
+        ]),
     },
     output: {
         filename: '[name]-dll.js',
@@ -26,7 +39,7 @@ const config = merge(core, {
             // The path to the manifest file which maps between
             // modules included in a bundle and the internal IDs
             // within that bundle
-            path: BUILD+'/[name]-dll-manifest.json',
+            path: BUILD + '/[name]-dll-manifest.json',
             // The name of the global variable which the library's
             // require function has been assigned to. This must match the
             // output.library option above
