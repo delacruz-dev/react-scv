cb0edfe1d9459d89f3d70c2a623072310b9baaf5
Marcello Montemagno <marcello.montemagno@workshare.com>
2017-06-12 11:06:47 +0100

    enable jest to run from scv when used as a global install

--- config/jest.js
+++ config/jest.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const CWD = process.cwd();
+const CWD_NODE_MODULES = path.join(CWD,'node_modules');
+const NODE_MODULES = path.join(__dirname, '../node_modules')
 const JEST_BABEL_TRANSFORMER = path.join(__dirname, '../utils/jestBabelTransformer.js');
 const JEST_STYLE_MOCK = path.join(__dirname, '../utils/styleMock.js');
 const JEST_FILE_MOCK = path.join(__dirname, '../utils/fileMock.js');
@@ -11,6 +13,7 @@ module.exports = {
     transform: {".*": JEST_BABEL_TRANSFORMER},
     collectCoverageFrom: ["src/**/*.js", "!**/node_modules/**", "!**/vendor/**"],
     testMatch: [ '**/__tests__/**/*.js?(x)', '**/?(*.)(spec|test).js?(x)', '**/*+(_spec|_test).js?(x)' ],
+    moduleDirectories: [CWD_NODE_MODULES, NODE_MODULES],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": JEST_FILE_MOCK,
         "\\.(css|less|scss|cssm|scssm)$": JEST_STYLE_MOCK
