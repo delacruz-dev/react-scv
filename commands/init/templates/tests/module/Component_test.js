5eb27177c5309a5ec37f937f9a30e3e0838f3c81
Marcello Montemagno <marcello.montemagno@workshare.com>
2017-09-26 16:22:23 +0200

    test update

--- commands/init/templates/tests/module/Component_test.js
+++ commands/init/templates/tests/module/Component_test.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import Component from '../../src/module/Component';
-
-const expect = chai.expect;
-
-chai.use(chaiEnzyme());
 
 describe('a test', () => {
 
   it(`hello world in a h1`, () => {
     const wrapper = shallow(<Component />);
     const h1 = wrapper.find('h1');
-
-    expect(h1).to.have.length(1);
-    expect(h1.text()).to.equal('Hello world');
+    expect(h1.text()).toBe('Hello world');
   });
 
 });
