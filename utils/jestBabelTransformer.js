e477aa37e1fb8a74b1d4682daf555695f6735306
Marcello Montemagno <marcello.montemagno@workshare.com>
2017-10-23 15:06:25 +0100

    test:debug to debug jest in chrome

--- utils/jestBabelTransformer.js
+++ utils/jestBabelTransformer.js
@@ -1,3 +1,10 @@
-const babelConfigObj = require('../config/babel.js');
-
-module.exports = require('babel-jest').createTransformer(babelConfigObj);
+module.exports = require('babel-jest').createTransformer({
+  presets: [
+    require.resolve('babel-preset-es2015'),
+    require.resolve('babel-preset-flow'),
+    require.resolve('babel-preset-stage-0'),
+    require.resolve('babel-preset-react')
+  ],
+  plugins: ["react-hot-loader/babel"],
+  sourceMap: "inline"
+});
