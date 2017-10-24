40834fdafdd32c0cb43579de56eff8369cc81c85
Marcello Montemagno <marcello.montemagno@workshare.com>
2017-10-24 17:22:36 +0100

    fix: avoid dll build when installing a scopes npm dependency created with scv

--- commands/dll/index.js
+++ commands/dll/index.js
@@ -19,6 +19,16 @@ module.exports = (args, done) => {
 };
 
 function isRunningOnADependency () {
+  return prevDirNameIsNodeModules() || prevDirIsAScopedModuleFolder();
+}
+
+function prevDirNameIsNodeModules () {
   const prevDirName = path.basename(path.join(process.cwd(), '../'));
   return prevDirName === "node_modules";
+}
+
+function prevDirIsAScopedModuleFolder () {
+  const prevDirName = path.basename(path.join(process.cwd(), '../'));
+  const prevPrevDirName = path.basename(path.join(process.cwd(), '../../'));
+  return prevDirName.substring(0, 1) === "@" && prevPrevDirName === "node_modules";
 }
