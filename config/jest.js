6a97b78d78e17a715c293730bb4e0d524f163a72
Daniel de la Cruz <daniel.delacruz@workshare.com>
2017-11-13 21:36:06 +0100

    Allow watch source files

--- config/jest.js
+++ config/jest.js
@@ -8,18 +8,20 @@ const JEST_FILE_MOCK = path.join(__dirname, '../utils/fileMock.js');
 const ENZYME_SETUP = path.join(__dirname, 'enzyme.js');
 const RAF_POLYFILL = require.resolve('raf/polyfill');
 const ROOT = path.join(CWD, 'tests');
+const SOURCES = path.join(CWD, 'src');
 
-module.exports = {
-  verbose: true,
-  rootDir: ROOT,
-  transform: {".*": JEST_BABEL_TRANSFORMER},
+module.exports = {    
   collectCoverageFrom: ["src/**/*.js", "!**/node_modules/**", "!**/vendor/**"],
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)(spec|test).js?(x)', '**/*+(_spec|_test).js?(x)'],
   moduleDirectories: [CWD_NODE_MODULES, NODE_MODULES],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": JEST_FILE_MOCK,
+  rootDir: ROOT,
+  roots: [SOURCES, ROOT],
+  setupFiles: [RAF_POLYFILL, ENZYME_SETUP],
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)(spec|test).js?(x)', '**/*+(_spec|_test).js?(x)'],
+  transform: {".*": JEST_BABEL_TRANSFORMER},
     "\\.(css|less|scss)$": JEST_STYLE_MOCK,
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": JEST_FILE_MOCK,
     "\\.(cssm|scssm)$": "identity-obj-proxy"
   },
-  setupFiles: [RAF_POLYFILL, ENZYME_SETUP]
+  verbose: true
 }
